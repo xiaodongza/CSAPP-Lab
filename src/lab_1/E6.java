4821884d6f414179cb9fabe16e9bcd7144a1df8c
xiaodongza <398186800@qq.com>
2021-11-09 15:52:10 +0800

    lab_1 e6

--- src/lab_1/E6.java
+++ src/lab_1/E6.java
@@ -2,10 +2,17 @@ package lab_1;
 
 public class E6 {
     public static void main(String[] args) {
-
+        System.out.println(isAsciiDigit(0x32));
     }
-
-    static int isAsciiDigit(int x){
-        return 0;
+    static boolean isAsciiDigit(int x){
+        int sign = 1<<31;
+        int upperBound = ~(sign|0x39);
+        int lowerBound = ~0x30;
+        upperBound = (upperBound+x)>>>31;
+        lowerBound = (lowerBound+1+x)>>>31;
+        //return lowerBound;
+        if(upperBound == 0 && lowerBound == 0)
+            return true;
+        return false;
     }
 }
